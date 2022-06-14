2674c5ccdc38961bb4cb8ba4113f580520b9100f
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-06-14 14:29:54 +0530

    Create variables.swift

---
+++ variables.swift
@@ -0,0 +1,43 @@
+import UIKit
+import Foundation
+var string="om namo venkateshaya"
+let str="harshaa reddy"
+string="shri vishn rupaya namah shivaya shri matrae namaha"
+print(string)
+print(str)
+var age=23 // int
+var height=15.3 // double / float
+var population=8_000_000 // for large numbers you can use like this for seperation of thousands
+print(age)
+print(height)
+print(population)
+var str2="""
+this is a demonstration of multi
+line string
+demonstrated using thrree single line strings
+"""
+print(str2)
+var istrue=true // boolean interpretaion
+if (istrue){
+    print("YES")}
+// string interpolation (means that adding a string in another string
+var score=20
+print("your score is \(score)")
+let s="fixed value" // let, unlike var cannot change at all
+// you can make the constants using let and variables using var
+// type annotations
+// when you want the variable to be of a particular type
+let name:String="harshaa reddy godlaveeti"
+let year:Int=2022
+let sankalpam:String="i will stand on my words"
+let areyousure:Bool=true
+print(name)
+print(year)
+
+
+
+
+
+
+
+
